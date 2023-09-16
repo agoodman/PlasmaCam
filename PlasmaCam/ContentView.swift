89c7481cf27644a4df2679cf189db49b143a80f9
Aubrey Goodman <aubrey.goodman@gmail.com>
2023-09-16 10:05:22 -0700

    Initial Commit

---
+++ PlasmaCam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlasmaCam
+//
+//  Created by Aubrey Goodman on 9/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlasmaCam/PlasmaCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlasmaCamApp.swift
+//  PlasmaCam
+//
+//  Created by Aubrey Goodman on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PlasmaCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
